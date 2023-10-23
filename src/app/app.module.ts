3ecb8d2f503bcf543d90852e15cec72741f8b7d5
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-10-23 07:01:56 -0300

    First video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { AboutComponent } from './views/about/about.component';
-import { PortfolioComponent } from './views/portfolio/portfolio.component';
-import { BlogComponent } from './views/blog/blog.component';
-import { ContactComponent } from './views/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './views/about/about.component';
+import { BlogComponent } from './views/blog/blog.component';
+import { PortfolioComponent } from './views/portfolio/portfolio.component';
+import { ContactComponent } from './views/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AboutComponent,
-    PortfolioComponent,
-    BlogComponent,
-    ContactComponent,
     FooterComponent,
+    AboutComponent,
+    BlogComponent,
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
