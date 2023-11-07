0ecbcf5222a0f4b92fac9048806c4c698d21454f
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-11-07 10:32:48 -0300

    Typed JS implemented and style changed

--- src/app/views/about/about.component.ts
+++ src/app/views/about/about.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent {
+  ngOnInit() {
+    const options = {
+      strings: [
+        'Web Developer',
+        'Angular Developer',
+        'Designer'],
+      typeSpeed: 100,
+      backSpeed: 70,
+      backDelay: 1000,
+      loop: true
+    };
+    const typed = new Typed('.typed-element', options);
+  }
 }
