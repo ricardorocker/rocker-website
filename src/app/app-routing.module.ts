ed26cd05cd65b9d1b802bcc76440bd550eaf814f
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-11-07 11:14:46 -0300

    Updated to be responsibness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,22 +4,22 @@ import { AboutComponent } from './views/about/about.component';
 import { ContactComponent } from './views/contact/contact.component';
 import { BlogComponent } from './views/blog/blog.component';
 import { PortfolioComponent } from './views/portfolio/portfolio.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+// import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ExperienceComponent } from './views/experience/experience.component';
 
 const routes: Routes = [
-  { path: 'about', title: 'about', component: AboutComponent },
+  { path: 'home', title: 'home', component: AboutComponent },
   { path: 'portfolio', title: 'portfolio', component: PortfolioComponent },
   { path: 'blog', title: 'blog', component: BlogComponent },
   { path: 'contact', title: 'contact', component: ContactComponent },
   { path: 'experience', title: 'experience', component: ExperienceComponent },
-  { path: 'about', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'home', redirectTo: '/home', pathMatch: 'full' },
   { path: 'portfolio', redirectTo: '/portfolio', pathMatch: 'full' },
   { path: 'blog', redirectTo: '/blog', pathMatch: 'full' },
   { path: 'contact', redirectTo: '/contact', pathMatch: 'full' },
   { path: 'experience', redirectTo: '/experience', pathMatch: 'full' },
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
